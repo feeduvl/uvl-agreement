8a591ec87be44c800d86f3384dee8d5b69754388
emilydickins <eileen.dickson@hotmail.de>
2022-02-08 09:52:08 +0100

    Speeding up initialization

--- status_updater.go
+++ status_updater.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type CodeMergeCandidate struct {
 	Tokens                    []*int
 	Name                      string
@@ -16,9 +14,7 @@ func updateStatusOfCodeAlternatives(
 ) []CodeAlternatives {
 	var mergeCandidates []CodeMergeCandidate
 	var rejected [][]*int
-	fmt.Printf("Number of codeAlternatives: %v\n\n", len(codeAlternatives))
 	for _, codeAlternative := range codeAlternatives {
-		fmt.Printf("Searching through codeAlternative %v\n\n", codeAlternative)
 		if len(mergeCandidates) == 0 {
 			if len(rejected) == 0 {
 				var newCandidate = CodeMergeCandidate{
@@ -34,12 +30,8 @@ func updateStatusOfCodeAlternatives(
 			}
 		} else {
 			for i, candidate := range mergeCandidates {
-				fmt.Printf("Number of candidates %v\n\n", len(mergeCandidates))
-				fmt.Printf("Index of candidate %v\n\n", i)
-				//fmt.Printf("Comparing to candidate %v\n\n", candidate)
 				if testEqSlice(codeAlternative.Code.Tokens, candidate.Tokens) {
 					if (codeAlternative.Code.Tore != candidate.Tore) || (codeAlternative.Code.Name != candidate.Name) || (!testEqSlice(codeAlternative.Code.RelationshipMemberships, candidate.RelationshipMemberships)) {
-						//fmt.Printf("%v already found\n\n", codeAlternative)
 						rejected = append(rejected, candidate.Tokens)
 						mergeCandidates = append(mergeCandidates[:i], mergeCandidates[i+1:]...)
 					} else {
@@ -50,7 +42,6 @@ func updateStatusOfCodeAlternatives(
 							}
 						}
 						if isNew {
-							//fmt.Printf("%v new, is added\n\n", codeAlternative)
 							mergeCandidates[i].annotationNameOccurrences = append(mergeCandidates[i].annotationNameOccurrences, codeAlternative.AnnotationName)
 							break
 						}
@@ -73,7 +64,6 @@ func setCodeMergeStatus(
 ) []CodeAlternatives {
 
 	for _, candidate := range mergeCandidates {
-		fmt.Printf("Setting status of MergeCandidate %v\n\n", candidate)
 		if len(candidate.annotationNameOccurrences) == numberOfAnnotations {
 			var isAccepted = false
 			for i, codeAlternative := range codeAlternatives {
@@ -104,8 +94,6 @@ func testCodeRejection(
 		if testEqSlice(codeAlternative.Code.Tokens, reject) {
 			isAReject = true
 			rejected = append(rejected, codeAlternative.Code.Tokens)
-			fmt.Printf("MergeCandidate %v\n", mergeCandidates)
-			fmt.Printf("len(MergeCandidate) %v\n", len(mergeCandidates))
 			for i, candidate := range mergeCandidates {
 				if testEqSlice(codeAlternative.Code.Tokens, candidate.Tokens) {
 					mergeCandidates = append(mergeCandidates[:i], mergeCandidates[i+1:]...)
